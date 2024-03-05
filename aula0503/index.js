92a661f4e241d4cb49f0a220e52e1f31c3ec443a
vitorhugocalejuri <vitor.silva1160@etec.sp.gov.br>
2024-03-05 20:36:14 -0300

    Aula 0503 de File System NodeJs

---
+++ aula0503/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const http = require('http')
+const port = 3000
+
+const server =http.createServer((req, res) =>{
+    fs.readFile('mensagem.html', function(err, data){
+        if(!data){
+            res.writeHead(500, {'Content-Type': 'text/html'})
+            res.write("<p>"+err+"</p>")
+            return res.end()
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        return res.end()
+    })
+})
+
+server.listen(port, () =>{
+    console.log('Funfando na porta: ' + port)
+})
