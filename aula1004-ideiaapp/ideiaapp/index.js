b5eacc2610d03bc04a9ef51dc732803c395c9775
vitorhugocalejuri <vitor.silva1160@etec.sp.gov.br>
2024-04-10 22:36:23 -0300

    Aula1004 estrutura de Express e HandleBars

---
+++ aula1004-ideiaapp/ideiaapp/index.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
