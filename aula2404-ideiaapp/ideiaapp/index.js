356c2326fb27d05a1047b31e68432e59199bc628
vitorhugocalejuri <vitor.silva1160@etec.sp.gov.br>
2024-04-24 21:37:09 -0300

    Aula2404 Entrega

---
+++ aula2404-ideiaapp/ideiaapp/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine','handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('layouts/main')
+})
+
+app.listen(3000, () => {
+    console.log('Servidor operando na porta local: http://127.0.0.1:3000')
+})
